b4cbac58763b6a4670fc66b954846db649fcbfac
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-10-22 13:27:29 +0200

    feat: padding for mobile

--- apps/discjakt-web/src/layout/Layout.tsx
+++ apps/discjakt-web/src/layout/Layout.tsx
@@ -7,7 +7,7 @@ type Props = React.PropsWithChildren<{}>;
 
 const Layout: React.FC<Props> = ({ children }) => {
   return (
-    <div className="min-h-screen flex flex-col pb-6 md:pb-0">
+    <div className="min-h-screen flex flex-col pt-16 pb-20 md:py-0">
       <Navbar />
 
       <div className="flex-auto flex flex-col">{children}</div>
