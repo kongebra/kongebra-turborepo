ff33b423b41558e2c8e65f1c62fe0e18b6dcdb92
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-11-01 08:56:07 +0100

    feat: instock filter

--- apps/discjakt-web/src/pages/stores/[slug]/index.tsx
+++ apps/discjakt-web/src/pages/stores/[slug]/index.tsx
@@ -1,16 +1,18 @@
-import { Store } from "@prisma/client";
+import { Disc, Product, Store } from "@prisma/client";
 import { GetStaticPaths, GetStaticProps, NextPage } from "next";
-import React from "react";
+import React, { useCallback, useMemo, useState } from "react";
 import { prisma } from "src/common/lib/prisma";
 
 import {
   Container,
+  FormLabel,
   Heading,
   LoadingPage,
   Section,
   SelectDiscSort,
   SimpleProduct,
 } from "src/frontend/components";
+import Toggle from "src/frontend/components/Toggle";
 import { useSortDiscs } from "src/frontend/hooks";
 import useStoreDiscs from "src/frontend/hooks/use-store-discs";
 
@@ -64,14 +66,46 @@ const StoreBySlugPage: NextPage<Props> = ({ store }) => {
   const { sort, setSort, sortFn } = useSortDiscs();
 
   const { discs, isInitialLoading } = useStoreDiscs(store.id);
+  const [showOnlyInStock, setShowOnlyInStock] = useState(true);
+
+  const isInStock = useCallback((disc: Disc & { products: Product[] }) => {
+    const prices = disc.products
+      .map((product) => product.latestPrice)
+      .filter((price) => price);
+
+    const lowest = Math.min(...prices);
+
+    if (lowest === Infinity) {
+      return false;
+    }
+
+    return lowest > 0;
+  }, []);
+
+  const filteredDiscs = useMemo(() => {
+    if (showOnlyInStock) {
+      return discs.filter(isInStock);
+    }
+
+    return discs;
+  }, [discs, showOnlyInStock]);
 
   return (
     <>
       <Section>
         <Container className="flex flex-col lg:flex-row justify-between lg:items-center mb-4 lg:mb-16">
           <Heading className="mb-2 lg:mb-0">{store.name}</Heading>
-          <div>
+
+          <div className="flex flex-col">
             <SelectDiscSort value={sort} onChange={setSort} />
+
+            <div className="flex flex-row-reverse lg:flex-col justify-end gap-2">
+              <FormLabel>Vis kun disker på lager</FormLabel>
+              <Toggle
+                defaultValue={showOnlyInStock}
+                onChange={setShowOnlyInStock}
+              />
+            </div>
           </div>
         </Container>
       </Section>
@@ -82,7 +116,7 @@ const StoreBySlugPage: NextPage<Props> = ({ store }) => {
         ) : (
           <Container>
             <div className="grid grid-cols-2 lg:grid-cols-6 gap-4">
-              {discs.sort(sortFn).map((disc) => (
+              {filteredDiscs.sort(sortFn).map((disc) => (
                 <SimpleProduct key={disc.id} disc={disc as any} />
               ))}
             </div>
